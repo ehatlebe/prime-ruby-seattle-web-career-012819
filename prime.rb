cda773645f620ccbc7fcd552c239fcd9356b5397
github username <github email address>
2019-01-17 23:19:33 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false # integer is not prime
+  end
+
+  for x in 2..(integer - 1)
+    if (integer % x) == 0
+      return false # integer is not prime
+    end
+  end
+
+  true # integer is prime
+end
